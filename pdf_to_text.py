6ebf54602910a1715b2011b0f566fd315d7b91dd
Ankush Rana <ankush.ran13@gmail.com>
2019-07-07 20:06:31 +0200

    Got patient?s gender

---
+++ pdf_to_text.py
@@ -0,0 +1,35 @@
+"""
+Extract PDF text using PDFMiner. Adapted from
+http://stackoverflow.com/questions/5725278/python-help-using-pdfminer-as-a-library
+"""
+
+from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LAParams, LTTextBox, LTTextLine
+
+from io import StringIO
+
+def pdf_to_text(pdf_file):
+
+    parser = PDFParser(pdf_file)
+    doc = PDFDocument()
+    parser.set_document(doc)
+    doc.set_parser(parser)
+    doc.initialize('')
+    rsrcmgr = PDFResourceManager()
+    laparams = LAParams()
+    laparams.char_margin = 1.0
+    laparams.word_margin = 1.0
+    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    extracted_text = ''
+
+    for page in doc.get_pages():
+        interpreter.process_page(page)
+        layout = device.get_result()
+        for lt_obj in layout:
+            if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
+                extracted_text += lt_obj.get_text()
+
+    return extracted_text
---
+++ test.py
@@ -0,0 +1,3 @@
+
+
+print("cid:105".to_unichar())
